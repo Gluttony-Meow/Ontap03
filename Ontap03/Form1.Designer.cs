cb88e81696ee6580a1ba041b1885053094e87ac7
Gluttony-Meow <glutony0910@gmail.com>
2021-08-10 09:25:04 +0700

    Initial commit

---
+++ Ontap03/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Ontap03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbQuantity = new System.Windows.Forms.TextBox();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.tbCustName = new System.Windows.Forms.TextBox();
+            this.tbCustMobile = new System.Windows.Forms.TextBox();
+            this.tbCustAddress = new System.Windows.Forms.TextBox();
+            this.OrderId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ProductId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Note = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CustName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CustMobile = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CustAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.OrderId,
+            this.ProductId,
+            this.Quantity,
+            this.Note,
+            this.CustName,
+            this.CustMobile,
+            this.CustAddress});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(691, 210);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(700, 25);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(80, 38);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Dữ Liệu";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(700, 282);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(76, 37);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 358);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(430, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(479, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // tbQuantity
+            // 
+            this.tbQuantity.Location = new System.Drawing.Point(76, 263);
+            this.tbQuantity.Name = "tbQuantity";
+            this.tbQuantity.Size = new System.Drawing.Size(130, 20);
+            this.tbQuantity.TabIndex = 9;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Location = new System.Drawing.Point(306, 263);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(146, 20);
+            this.tbNote.TabIndex = 10;
+            // 
+            // tbCustName
+            // 
+            this.tbCustName.Location = new System.Drawing.Point(531, 263);
+            this.tbCustName.Name = "tbCustName";
+            this.tbCustName.Size = new System.Drawing.Size(144, 20);
+            this.tbCustName.TabIndex = 11;
+            // 
+            // tbCustMobile
+            // 
+            this.tbCustMobile.Location = new System.Drawing.Point(169, 358);
+            this.tbCustMobile.Name = "tbCustMobile";
+            this.tbCustMobile.Size = new System.Drawing.Size(131, 20);
+            this.tbCustMobile.TabIndex = 12;
+            // 
+            // tbCustAddress
+            // 
+            this.tbCustAddress.Location = new System.Drawing.Point(482, 358);
+            this.tbCustAddress.Name = "tbCustAddress";
+            this.tbCustAddress.Size = new System.Drawing.Size(140, 20);
+            this.tbCustAddress.TabIndex = 13;
+            // 
+            // OrderId
+            // 
+            this.OrderId.DataPropertyName = "OrderId";
+            this.OrderId.HeaderText = "Id";
+            this.OrderId.Name = "OrderId";
+            // 
+            // ProductId
+            // 
+            this.ProductId.DataPropertyName = "ProdcutId";
+            this.ProductId.HeaderText = "ProductId";
+            this.ProductId.Name = "ProductId";
+            // 
+            // Quantity
+            // 
+            this.Quantity.DataPropertyName = "Quantity";
+            this.Quantity.HeaderText = "Quantity";
+            this.Quantity.Name = "Quantity";
+            // 
+            // Note
+            // 
+            this.Note.DataPropertyName = "Note";
+            this.Note.HeaderText = "Note";
+            this.Note.Name = "Note";
+            // 
+            // CustName
+            // 
+            this.CustName.DataPropertyName = "CustName";
+            this.CustName.HeaderText = "CustName";
+            this.CustName.Name = "CustName";
+            // 
+            // CustMobile
+            // 
+            this.CustMobile.DataPropertyName = "CustMobile";
+            this.CustMobile.HeaderText = "CustMobile";
+            this.CustMobile.Name = "CustMobile";
+            // 
+            // CustAddress
+            // 
+            this.CustAddress.DataPropertyName = "CustAddress";
+            this.CustAddress.HeaderText = "CustAddress";
+            this.CustAddress.Name = "CustAddress";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 435);
+            this.Controls.Add(this.tbCustAddress);
+            this.Controls.Add(this.tbCustMobile);
+            this.Controls.Add(this.tbCustName);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.tbQuantity);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbQuantity;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.TextBox tbCustName;
+        private System.Windows.Forms.TextBox tbCustMobile;
+        private System.Windows.Forms.TextBox tbCustAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OrderId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Note;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustMobile;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CustAddress;
+    }
+}
+
---
+++ Ontap03/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ontap03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cnn = new SqlConnection(@"Data Source=DESKTOP-GBBCS2R;Initial Catalog=bech;Integrated Security=True");
+        private void ketnoicsdl()
+        {
+            cnn.Open();
+            string sql = "select * from tblOrder";  // lay het du lieu trong bang sinh vien
+            SqlCommand com = new SqlCommand(sql, cnn); //bat dau truy van
+            SqlDataAdapter da = new SqlDataAdapter(com); //chuyen du lieu ve
+            DataTable dt = new DataTable(); //tạo một kho ảo để lưu trữ dữ liệu
+            da.Fill(dt);  // đổ dữ liệu vào kho
+            cnn.Close();  // đóng kết nối
+            dataGridView1.DataSource = dt; //đổ dữ liệu vào datagridview
+        }
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            ketnoicsdl();
+        }
+        private void add() {
+            string sqlInsert = "INSERT INTO tblOrder(Quantity ,Note, CustName, CustMobile, CustAddress) values (@Quantity,@Note,@CustName,@CustMobile, @CustAddress) ";
+            SqlCommand com = new SqlCommand(sqlInsert, cnn);
+            com.Parameters.AddWithValue("@Quantity", tbQuantity.Text);// dữ liệu 01
+            com.Parameters.AddWithValue("@Note", tbNote.Text);
+            com.Parameters.AddWithValue("@CustName", tbCustName.Text);
+            com.Parameters.AddWithValue("@CustMobile", tbCustMobile.Text);
+            com.Parameters.AddWithValue("@CustAddress", tbCustAddress.Text);
+            cnn.Open();
+            com.ExecuteNonQuery();
+            cnn.Close();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if ( Int32.Parse (tbQuantity.Text) < 0)
+                MessageBox.Show("Quantity must be greater than 0 ");       
+            if (tbCustName.Text == string.Empty)
+                MessageBox.Show("TextBox is Empty");
+            if (tbCustMobile.Text == string.Empty)
+                MessageBox.Show("TextBox is Empty");
+            if (tbCustAddress.Text == string.Empty)
+                MessageBox.Show("TextBox is Empty");
+            add();
+            ketnoicsdl();
+        }
+    }
+}
